7384b9d092a363e854a8c358d285b036bf3e374c
Frotrej <f.faszczowy@gmail.com>
2026-07-02 20:41:27 +0200

    Added more charts to get to know to do it

--- QuikGrapTest/MainWindow.xaml.cs
+++ QuikGrapTest/MainWindow.xaml.cs
@@ -29,14 +29,22 @@ namespace QuikGrapTest
 
 		private void MainWindow_Loaded(object? sender, RoutedEventArgs e)
 		{
+			CreateBasicPieChart();
 
+			CreateBasicScatterChart();
+
+			CreateBasicBarChart();
+		}
+
+		private void CreateBasicPieChart()
+		{
 			ScottPlot.Plot myPlot = new();
 
 			PieSlice slice1 = new() { Value = 80, FillColor = ScottPlot.Colors.Red, Label = "alpha", LegendText = "Alpha" };
 			PieSlice slice2 = new() { Value = 5, FillColor = ScottPlot.Colors.Orange, Label = "beta", LegendText = "Beta" };
-			PieSlice slice3 = new() { Value = 25, FillColor = ScottPlot.Colors.Gold, Label = "gamma"};
+			PieSlice slice3 = new() { Value = 25, FillColor = ScottPlot.Colors.Gold, Label = "gamma" };
 			PieSlice slice4 = new() { Value = 31, FillColor = ScottPlot.Colors.Green, Label = "delta", LegendText = "Delta" };
-			PieSlice slice5 = new() { Value = 13, FillColor = ScottPlot.Colors.Blue, Label = "epsilon"};
+			PieSlice slice5 = new() { Value = 13, FillColor = ScottPlot.Colors.Blue, Label = "epsilon" };
 
 			List<PieSlice> slices = new() { slice1, slice2, slice3, slice4, slice5 };
 
@@ -44,19 +52,38 @@ namespace QuikGrapTest
 			pie.ExplodeFraction = .1;
 
 			myPlot.HideAxesAndGrid();
-			
 
-			PiePlot.Reset(myPlot);
-			PiePlot.Refresh();
+			WPFPiePlot.Reset(myPlot);
+			WPFPiePlot.Refresh();
+		}
+		private void CreateBasicScatterChart()
+		{
+			ScottPlot.Plot myPlot = new();
 
+			string[] XValuesString = { "day1", "day2", "day3", "day4", "day5", "day6" };
+			double[] XValues = { 1, 2, 3, 4, 5, 6 };
+			double[] YValues = { 4, 30, 13, 25, 30, 35 };
 
-			//co to ma byc xD
-			// Disable mouse interactions that would pan/zoom/move the plot while the app is running
-			// Use Preview* so events are handled before ScottPlot's handlers run.
-			/*PiePlot.PreviewMouseDown += (s, ev) => ev.Handled = true;
-			PiePlot.PreviewMouseMove += (s, ev) => ev.Handled = true;
-			PiePlot.PreviewMouseUp += (s, ev) => ev.Handled = true;
-			PiePlot.PreviewMouseWheel += (s, ev) => ev.Handled = true;*/
+			myPlot.Add.Scatter(XValues, YValues);
+
+			WPFScatterPlot.Reset(myPlot);
+			WPFScatterPlot.Refresh();
+		}
+		private void CreateBasicBarChart()
+		{
+			ScottPlot.Plot myPlot = new();
+
+			double[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 };
+
+			var barPlot = myPlot.Add.Bars(values);
+
+			foreach (var bar in barPlot.Bars)
+			{
+				bar.Label = bar.Value.ToString();
+			}
+
+			WPFBarPlot.Reset(myPlot);
+			WPFBarPlot.Refresh();
 		}
 	}
 }
